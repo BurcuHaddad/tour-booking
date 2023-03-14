e8541f1be235df9417ebee13db8dca08318a0fa5
BurcuHaddad <burcuhaddad@gmail.com>
2023-03-14 16:27:24 +0300

    Authentication& Authorizaiton - Calculating average rating on tours

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -2,6 +2,8 @@ const fs = require("fs");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const Tour = require("./../../models/tourModels");
+const User = require("./../../models/userModels");
+const Review = require("./../../models/reviewModels");
 
 dotenv.config({ path: "./config.env" });
 
@@ -23,11 +25,19 @@ mongoose
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/tours.json`, "utf-8")
 );
+const users = JSON.parse(
+  fs.readFileSync(`${__dirname}/users.json`, "utf-8")
+);
+const reviews = JSON.parse(
+  fs.readFileSync(`${__dirname}/reviews.json`, "utf-8")
+);
 
 //IMPORT DATA INTO DB
 const importData = async () => {
   try {
     await Tour.create(tours);
+    await User.create(users, { validateBeforeSave: false });
+    await Review.create(reviews);
     console.log("Data succesfully loaded!");
 } catch (err) {
     console.log(err);
@@ -39,6 +49,8 @@ process.exit();
 const deleteData = async () => {
   try {
     await Tour.deleteMany();
+    await User.deleteMany();
+    await Review.deleteMany();
     console.log("Data succcesfully deleted!");
 } catch (err) {
     console.log(err);
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,24 +2,22 @@ const express = require("express");
 const userController = require("./../controllers/userController");
 const authController = require("./../controllers/authController");
 
-
 const router = express.Router();
 
 router.post("/signup", authController.signup);
 router.post("/login", authController.login);
-
 router.post("/forgotPassword", authController.forgotPassword);
 router.patch("/resetPassword/:token", authController.resetPassword);
 
-router.patch(
-  "/updateMyPassword",
-  authController.protect,
-  authController.updatePassword
-);
+//Protect all routes after this middleware
+router.use(authController.protect);
 
-router.patch("/updateMe", authController.protect, userController.updateMe)
+router.patch("/updateMyPassword", authController.updatePassword);
+router.get("/me", userController.getMe, userController.getUser);
+router.patch("/updateMe", userController.updateMe);
+router.delete("/deleteMe", userController.deleteMe);
 
-router.delete("/deleteMe", authController.protect, userController.deleteMe)
+router.use(authController.restrictTo("admin"))
 
 router
   .route("/")
@@ -31,7 +29,5 @@ router
   .get(userController.getUser)
   .patch(userController.updateUser)
   .delete(userController.deleteUser);
-
-
 
 module.exports = router;
